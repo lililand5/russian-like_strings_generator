241abb062700e684c98918ec34f1de05a1b79b46
Liliya <tor-ver@mail.ru>
2021-11-24 11:50:54 +0300

    Добавлены вероятностные константы для реализации актуального распределения букв

--- app/methods.rb
+++ app/methods.rb
@@ -1,7 +1,85 @@
- def rl_str_gen #russian like strings generator
+# Частота букв алфавита
+LETTERS_FREQ = {
+  1072 => 801, # а
+  1073 => 159, # б
+  1074 => 454, # в
+  1075 => 170, # г
+  1076 => 298, # д
+  1077 => 749, # е
+  1105 => 100, # ё
+  1078 => 94, # ж
+  1079 => 165, # з
+  1080 => 735, # и
+  1081 => 121, # й
+  1082 => 349, # к
+  1083 => 440, # л
+  1084 => 321, # м
+  1085 => 670, # н
+  1086 => 1097, # о
+  1087 => 281, # п
+  1088 => 473, # р
+  1089 => 547, # с
+  1090 => 626, # т
+  1091 => 262, # у
+  1092 => 26, # ф
+  1093 => 97, # х
+  1094 => 48, # ц
+  1095 => 144, # ч
+  1096 => 73, # ш
+  1097 => 36, # щ
+  1098 => 4, # ъ
+  1099 => 190, # ы  
+  1100 => 174, # ь
+  1101 => 32, # э
+  1102 => 64, # ю
+  1103 => 201 # я
+}.freeze
 
+
+# удельный вес букв "ивясакуо"
+ONE_LETTER_WORDS_FREQ = {
+  1080 => 358, # и
+  1074 => 314, # в
+  1103 => 127, # я
+  1089 => 113, # с
+  1072 => 82, # а
+  1082 => 54, # к
+  1091 => 43, # у
+  1086 => 34 # о
+}.freeze
+
+
+
+# вероятностный массив для однобуквенных слов
+# предоставь распределенный однобуквенный массив
+def provide_distribution(hash)
+  sample_array = []
+  hash.each_key do |k|
+
+    hash[k].times do 
+      sample_array << k
+    end
+    sample_array.freeze
+    sample_array
+  end
+end
+
+ONE_LETTER_WORDS_PROBABILITY_ARRAY = provide_distribution(ONE_LETTER_WORDS_FREQ)
+
+
+LETTERS_PROBABILITY_ARRAY          = provide_distribution(LETTERS_FREQ)
+
+
+
+
+
+
+
+
+# методы
+
+def rl_str_gen #russian like strings generator
   base_string
-
 end
 
 
@@ -18,8 +96,6 @@ def base_string
 
   # p arr.pack("U*")
   arr.pack("U*")
-
-end
 end
 
 
@@ -33,7 +109,6 @@ end
 def plan_words
   arr = Array.new(rand(2..15)) {{}}
   
-
     # arr[el] = {
     #   case: :downcase,
     #   multi_syllable: true,
@@ -108,13 +183,16 @@ end
 # В односложных словах не должно быть гласных в начале слова, 
 # если они состоят из 3 или более букв.
 
-def make_common_word(hash)
+
+
+def make_common_word(hash) 
 
   # :case
   if hash[:multi_syllable]
     word = generate_multi_syllable
   elsif hash[:one_letter]
-    word = [1072, 1103, 1074, 1086, 1091, 1080, 1082, 1089].sample
+    ONE_LETTER_WORDS_PROBABILITY_ARRAY.sample
+
   else
     word = generate_single_syllable_word
   end
@@ -127,6 +205,15 @@ def make_common_word(hash)
 end
 
 
+def generate_single_syllable_word
+
+  length = rand(20) < 15 ? rand(2..4) : rand(5..7)
+  vowel = [1072, 1086, 1091, 1101, 1099, 1080, 1103, 1077, 1105, 1102].sample
+
+
+  [1073, 1074, 1075, 1076, 1078, 1079, 1082, 1083, 1084, 1085, 1087, 1088, 1089, 1090, 1092, 1093, 1094, 1095, 1096, 1097]
+  
+end
 
 
 
